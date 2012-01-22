253650b153779ce8a3cf582668adabd7b247fed1
Srinivasan Raguraman <ThoughtWorks@blankslate-sr5977.local>
2012-01-21 16:48:32 -0800

    adding libraries, support commandline parsing.

---
+++ src/test/java/com/sragu/broch/MainTest.java
@@ -0,0 +1,18 @@
+package com.sragu.broch;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest  {
+
+  @Test
+  public void testApp() {
+    assertTrue( true );
+  }
+}
