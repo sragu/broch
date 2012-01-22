76dcca9bae7afe16816e808a49e69d1b8b2bec23
Srinivasan Raguraman <ThoughtWorks@blankslate-sr5977.local>
2012-01-22 10:52:06 -0800

    add commandline processing, and separate uber jar task.

--- src/main/java/com/sragu/broch/Main.java
+++ src/main/java/com/sragu/broch/Main.java
@@ -1,7 +1,24 @@
 package com.sragu.broch;
 
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
 public class Main {
-  public static void main( String[] args ) {
-    System.out.println( "Hello World!" );
-  }
+    public static void main(String[] args) {
+        BrochOptions bean = new BrochOptions();
+        CmdLineParser parser = new CmdLineParser(bean);
+
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+        }
+    }
+
+    static class BrochOptions {
+        @Option(name = "--init", usage = "sets up the build environment")
+        private boolean bootstrap;
+    }
 }
